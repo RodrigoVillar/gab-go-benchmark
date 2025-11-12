2467e2e0f8a92640155f5ef2baebee329cba30b1
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 14:10:28 +0000

    add Benchmark (go) benchmark result for 255505a5a4bc750a91d6d49220e29c2a8f1bb73c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762913645453,
+  "lastUpdate": 1762956628812,
   "repoUrl": "https://github.com/RodrigoVillar/gab-go-benchmark",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,72 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/RodrigoVillar/gab-go-benchmark/commit/cce6b45b0a1b11da78af661990948e56a3eb8084"
         },
         "date": 1762913645178,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib20WithBlockchainMetrics",
+            "value": 1,
+            "unit": "block_accept_ms/ggas",
+            "extra": "30134 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithBlockchainMetrics - block_parse_ms/ggas",
+            "value": 2,
+            "unit": "block_parse_ms/ggas",
+            "extra": "30134 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithBlockchainMetrics - block_verify_ms/ggas",
+            "value": 3,
+            "unit": "block_verify_ms/ggas",
+            "extra": "30134 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithBlockchainMetrics - mgas/s",
+            "value": 4,
+            "unit": "mgas/s",
+            "extra": "30134 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithBlockchainMetrics - ms/ggas",
+            "value": 5,
+            "unit": "ms/ggas",
+            "extra": "30134 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithBlockchainMetrics - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "30134 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithBlockchainMetrics - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "30134 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "committer": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "distinct": true,
+          "id": "255505a5a4bc750a91d6d49220e29c2a8f1bb73c",
+          "message": "chore: add useless comment",
+          "timestamp": "2025-11-12T09:09:51-05:00",
+          "tree_id": "8405039bd66c20df82e0c68a6d071bd2ef42aa5f",
+          "url": "https://github.com/RodrigoVillar/gab-go-benchmark/commit/255505a5a4bc750a91d6d49220e29c2a8f1bb73c"
+        },
+        "date": 1762956628001,
         "tool": "go",
         "benches": [
           {
