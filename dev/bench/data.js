c9d725a25a64dedbfbe40eda4f491b5cc1a87946
Rodrigo Villar <rodrigo.villar@avalabs.org>
2025-11-11 21:13:14 -0500

    chore: remove benchmark data again

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -2,67 +2,6 @@ window.BENCHMARK_DATA = {
   "lastUpdate": 1762913398122,
   "repoUrl": "https://github.com/RodrigoVillar/gab-go-benchmark",
   "entries": {
-    "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "rodrigo.villar@avalabs.org",
-            "name": "Rodrigo Villar",
-            "username": "RodrigoVillar"
-          },
-          "committer": {
-            "email": "rodrigo.villar@avalabs.org",
-            "name": "Rodrigo Villar",
-            "username": "RodrigoVillar"
-          },
-          "distinct": true,
-          "id": "2f509886b3805ccbea64faed751c43ba1d190875",
-          "message": "chore: replicate reexecution multi metrics",
-          "timestamp": "2025-11-11T21:09:29-05:00",
-          "tree_id": "10c68e0582408816b79c12418189824b34c0994d",
-          "url": "https://github.com/RodrigoVillar/gab-go-benchmark/commit/2f509886b3805ccbea64faed751c43ba1d190875"
-        },
-        "date": 1762913397326,
-        "tool": "go",
-        "benches": [
-          {
-            "name": "BenchmarkFib20WithBlockchainMetrics",
-            "value": 1,
-            "unit": "block_accept_ms/ggas",
-            "extra": "30135 times\n4 procs"
-          },
-          {
-            "name": "BenchmarkFib20WithBlockchainMetrics - block_parse_ms/ggas",
-            "value": 3,
-            "unit": "block_parse_ms/ggas",
-            "extra": "30135 times\n4 procs"
-          },
-          {
-            "name": "BenchmarkFib20WithBlockchainMetrics - mgas/s",
-            "value": 4,
-            "unit": "mgas/s",
-            "extra": "30135 times\n4 procs"
-          },
-          {
-            "name": "BenchmarkFib20WithBlockchainMetrics - ms/ggas",
-            "value": 5,
-            "unit": "ms/ggas",
-            "extra": "30135 times\n4 procs"
-          },
-          {
-            "name": "BenchmarkFib20WithBlockchainMetrics - B/op",
-            "value": 0,
-            "unit": "B/op",
-            "extra": "30135 times\n4 procs"
-          },
-          {
-            "name": "BenchmarkFib20WithBlockchainMetrics - allocs/op",
-            "value": 0,
-            "unit": "allocs/op",
-            "extra": "30135 times\n4 procs"
-          }
-        ]
-      }
-    ]
+    "Benchmark": []
   }
 }
