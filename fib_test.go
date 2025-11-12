cce6b45b0a1b11da78af661990948e56a3eb8084
Rodrigo Villar <rodrigo.villar@avalabs.org>
2025-11-11 21:13:41 -0500

    fix: duplicate metrics

--- fib_test.go
+++ fib_test.go
@@ -12,7 +12,7 @@ func BenchmarkFib20WithBlockchainMetrics(b *testing.B) {
 	b.ReportMetric(0, "ns/op")
 	b.ReportMetric(1.0, "block_accept_ms/ggas")
 	b.ReportMetric(2.0, "block_parse_ms/ggas")
-	b.ReportMetric(3.0, "block_parse_ms/ggas")
+	b.ReportMetric(3.0, "block_verify_ms/ggas")
 	b.ReportMetric(4.0, "mgas/s")
 	b.ReportMetric(5.0, "ms/ggas")
 }
