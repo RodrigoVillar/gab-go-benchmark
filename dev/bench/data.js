267017be04085a4a894913f31e905a86c3f0e817
github-action-benchmark <github@users.noreply.github.com>
2025-12-01 16:12:50 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 7c15a3d3272e6a2099d7165e2739f9cf18e06abf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762956628812,
+  "lastUpdate": 1764605570624,
   "repoUrl": "https://github.com/RodrigoVillar/gab-go-benchmark",
   "entries": {
     "Benchmark": [
@@ -132,6 +132,55 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "30134 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "committer": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "distinct": true,
+          "id": "7c15a3d3272e6a2099d7165e2739f9cf18e06abf",
+          "message": "chore: use custom benchmarks",
+          "timestamp": "2025-12-01T11:12:10-05:00",
+          "tree_id": "b9cf9b768b2e82ee4673084798fb761b94a603e9",
+          "url": "https://github.com/RodrigoVillar/gab-go-benchmark/commit/7c15a3d3272e6a2099d7165e2739f9cf18e06abf"
+        },
+        "date": 1764605569810,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Alpha",
+            "value": 1.1,
+            "unit": "block_accept_ms/ggas"
+          },
+          {
+            "name": "Beta",
+            "value": 2.2,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "Charlie",
+            "value": 3.3,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "Delta",
+            "value": 4,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "Echo",
+            "value": 5,
+            "unit": "ms/ggas"
           }
         ]
       }
