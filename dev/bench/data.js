24d0331bfb1fb7b2ccc064faa4f063f415d48ad1
github-action-benchmark <github@users.noreply.github.com>
2025-12-01 16:17:24 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5f0a2f56aa85fb0ef8274286b5e82d5220239e5e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764605570624,
+  "lastUpdate": 1764605844176,
   "repoUrl": "https://github.com/RodrigoVillar/gab-go-benchmark",
   "entries": {
     "Benchmark": [
@@ -181,6 +181,50 @@ window.BENCHMARK_DATA = {
             "name": "Echo",
             "value": 5,
             "unit": "ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "committer": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "distinct": true,
+          "id": "5f0a2f56aa85fb0ef8274286b5e82d5220239e5e",
+          "message": "chore: nit",
+          "timestamp": "2025-12-01T11:17:00-05:00",
+          "tree_id": "30db87bb6762ce06fa20c07ce2358a564f225014",
+          "url": "https://github.com/RodrigoVillar/gab-go-benchmark/commit/5f0a2f56aa85fb0ef8274286b5e82d5220239e5e"
+        },
+        "date": 1764605843359,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Alpha",
+            "value": 1.1,
+            "unit": "block_accept_ms/ggas"
+          },
+          {
+            "name": "Beta",
+            "value": 2.2,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "Charlie",
+            "value": 3.3,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "Delta",
+            "value": 4,
+            "unit": "mgas/s"
           }
         ]
       }
