59be5c5ba67b8b1e7875abdde41fbff3a3d56784
github-action-benchmark <github@users.noreply.github.com>
2025-12-01 16:21:14 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 6b945f5ed317fa6fb5ba745e2a56d60c46deebf5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764605844176,
+  "lastUpdate": 1764606074675,
   "repoUrl": "https://github.com/RodrigoVillar/gab-go-benchmark",
   "entries": {
     "Benchmark": [
@@ -72,6 +72,50 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/RodrigoVillar/gab-go-benchmark/commit/5f0a2f56aa85fb0ef8274286b5e82d5220239e5e"
         },
         "date": 1764605843359,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Alpha",
+            "value": 1.1,
+            "unit": "block_accept_ms/ggas"
+          },
+          {
+            "name": "Beta",
+            "value": 2.2,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "Charlie",
+            "value": 3.3,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "Delta",
+            "value": 4,
+            "unit": "mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "committer": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "distinct": true,
+          "id": "6b945f5ed317fa6fb5ba745e2a56d60c46deebf5",
+          "message": "chore: add TODO",
+          "timestamp": "2025-12-01T11:20:51-05:00",
+          "tree_id": "5c0db4a924b18105329887f3cb05b71ef52f10fd",
+          "url": "https://github.com/RodrigoVillar/gab-go-benchmark/commit/6b945f5ed317fa6fb5ba745e2a56d60c46deebf5"
+        },
+        "date": 1764606073805,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
